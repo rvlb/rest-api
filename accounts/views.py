8331a22ae30bef8dfa9307b9e68e7651a0c2b6eb
rvlb-19 <rvlb@cin.ufpe.br>
2017-12-17 18:16:35 -0300

    repo init

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework import viewsets
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from rest_framework.routers import DefaultRouter
+
+from accounts.routes import routes as accounts_routes
+
+router = DefaultRouter()
+
+routes = accounts_routes
+for route in routes:
+    router.register(route[0], route[1])
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
